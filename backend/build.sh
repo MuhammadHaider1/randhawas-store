f075370f93662c86cd4cc363e712d78065825199
Muhammed Haider <muhammedhaider914@gmail.com>
2026-07-01 11:49:46 +0500

    Fix admin login: set is_admin_user=True for Hira-R

--- backend/build.sh
+++ backend/build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py seed_categories
-echo "from django.contrib.auth import get_user_model; User=get_user_model(); user,_=User.objects.get_or_create(username='Hira-R', defaults={'is_superuser':True,'is_staff':True}); user.set_password('HiraRandhawastore2026'); user.is_superuser=True; user.is_staff=True; user.save()" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User=get_user_model(); user,_=User.objects.get_or_create(username='Hira-R', defaults={'is_superuser':True,'is_staff':True,'is_admin_user':True}); user.set_password('HiraRandhawastore2026'); user.is_superuser=True; user.is_staff=True; user.is_admin_user=True; user.save()" | python manage.py shell
